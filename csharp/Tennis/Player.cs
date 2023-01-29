974a316f68902108dbb715bca799fdac56669f37
Adeola Idowu <adeolaidowu27@gmail.com>
2023-01-29 21:56:12 +0100

    create a new Player model to hold player data and utilize in TennisGame3.cs.

---
+++ csharp/Tennis/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tennis
+{
+    public class Player
+    {
+        public Player(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+        public int Points { get; set; }
+    }
+}
--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -4,13 +4,13 @@ namespace Tennis
     {
         private int p2;
         private int p1;
-        private string p1N;
-        private string p2N;
+        private readonly Player p1N;
+        private readonly Player p2N;
 
         public TennisGame3(string player1Name, string player2Name)
         {
-            this.p1N = player1Name;
-            this.p2N = player2Name;
+            this.p1N.Name = player1Name;
+            this.p2N.Name = player2Name;
         }
 
         public string GetScore()
@@ -26,7 +26,7 @@ namespace Tennis
             {
                 if (p1 == p2)
                     return "Deuce";
-                s = p1 > p2 ? p1N : p2N;
+                s = p1 > p2 ? p1N.Name : p2N.Name;
                 return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
             }
         }
